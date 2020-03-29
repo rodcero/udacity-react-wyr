e703dbdcb4e86d9b252ec813b4e44461a6108ad8
ernie <2838152+rodcero@users.noreply.github.com>
2020-03-28 22:10:24 -0700

    feature: added display functionality to leaderboard component

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class Leaderboard extends Component {
+import './Leaderboard.css';
+
+class Leaderboard extends Component {
   static propTypes = {
-    prop: PropTypes,
+    users: PropTypes.array.isRequired,
+    user: PropTypes.string.isRequired,
   };
 
   render() {
-    return <div>Leaderboard</div>;
+    return (
+      <div>
+        <h3>Leaderboard</h3>
+        {this.props.users.map(user => (
+          <div key={user.id} className="leaderboard-user">
+            <div className="leaderboard-score">
+              <span>score</span>
+              <div>{user.questionCount + user.answerCount}</div>
+            </div>
+            <div className="leaderboard-content">
+              <div>Avatar</div>
+              <div className="leaderboard-details">
+                <h4>{user.name}</h4>
+                <div>Questions: {user.questionCount}</div>
+                <div>Answers: {user.answerCount}</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
+
+const mapState = ({ users, user }) => {
+  const userList = Object.keys(users).map(userId => {
+    const user = users[userId];
+    const questionCount = Object.keys(user.questions).length;
+    const answerCount = Object.keys(user.answers).length;
+    return {
+      id: user.id,
+      name: user.name,
+      avatarUrl: user.avatarUrl,
+      questionCount,
+      answerCount,
+      score: questionCount + answerCount,
+    };
+  });
+
+  return { users: userList.sort((a, b) => b.score - a.score), user };
+};
+
+export default connect(mapState)(Leaderboard);
