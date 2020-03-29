df1229f800d84a7a2330155cb81a328bc9656c64
ernie <2838152+rodcero@users.noreply.github.com>
2020-03-29 13:57:37 -0700

    feature: made navbar aware of current url for styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 import './NavBar.css';
 
-export default function NavBar() {
+function NavBar({ location }) {
   return (
     <div className="navbar">
-      <Link to="/">HOME</Link>
-      <Link to="/add">NEW QUESTION</Link>
-      <Link to="/leaderboard">LEADERBOARD</Link>
+      <Link className={location.pathname === '/' && 'selected'} to="/">
+        HOME
+      </Link>
+      <Link className={location.pathname === '/add' && 'selected'} to="/add">
+        NEW QUESTION
+      </Link>
+      <Link
+        className={location.pathname === '/leaderboard' && 'selected'}
+        to="/leaderboard"
+      >
+        LEADERBOARD
+      </Link>
     </div>
   );
 }
+
+export default withRouter(NavBar);
