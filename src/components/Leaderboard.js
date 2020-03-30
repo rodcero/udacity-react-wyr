c4ed2c1d55c6a62410b50bd7403da7bfd767ebd0
ernie <2838152+rodcero@users.noreply.github.com>
2020-03-30 12:16:40 -0700

    feature: added marker to identify the current user's position in the leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -3,48 +3,32 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import './Leaderboard.css';
+import LeaderboardItem from './LeaderboardItem';
 
 class Leaderboard extends Component {
   static propTypes = {
     users: PropTypes.array.isRequired,
-    user: PropTypes.string.isRequired,
+    authedUserId: PropTypes.string.isRequired,
   };
 
   render() {
+    const { authedUserId } = this.props;
     return (
       <div>
         <h1>Leaderboard</h1>
         {this.props.users.map(user => (
-          <div
+          <LeaderboardItem
+            user={user}
+            isCurrentUser={authedUserId === user.id}
             key={user.id}
-            className={`leaderboard-user ${
-              this.props.user === user.id ? ' current' : ''
-            }`}
-          >
-            <div className="leaderboard-score">
-              <span>score</span>
-              <div>{user.questionCount + user.answerCount}</div>
-            </div>
-            <div className="leaderboard-content">
-              <div className={`avatar ${user.avatarUrl}`}></div>
-              <div className="leaderboard-details">
-                <div className="name">{user.name}</div>
-                <div className="detail">
-                  <span>created questions:</span> {user.questionCount}
-                </div>
-                <div className="detail">
-                  <span>answers questions:</span> {user.answerCount}
-                </div>
-              </div>
-            </div>
-          </div>
+          />
         ))}
       </div>
     );
   }
 }
 
-const mapState = ({ users, user }) => {
+const mapState = ({ users, auth }) => {
   const userList = Object.keys(users).map(userId => {
     const user = users[userId];
     const questionCount = Object.keys(user.questions).length;
@@ -59,7 +43,10 @@ const mapState = ({ users, user }) => {
     };
   });
 
-  return { users: userList.sort((a, b) => b.score - a.score), user };
+  return {
+    users: userList.sort((a, b) => b.score - a.score),
+    authedUserId: auth.userId,
+  };
 };
 
 export default connect(mapState)(Leaderboard);
